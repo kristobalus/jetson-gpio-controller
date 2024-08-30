892930381303f5e4d44a3d835c96f4afcce40f29
kristobalus <ildar.tamendarov@gmail.com>
2024-08-30 17:06:53 +0500

    update

--- forward.py
+++ forward.py
@@ -42,8 +42,9 @@ def backward_to_stop():
 
 
 try:
+    pwm.ChangeDutyCycle(full_forward)
     # while True:
-    forward()
+    # forward()
     # forward_to_stop()
     # time.sleep(1)
     # backward()
