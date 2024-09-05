21bbe5a95bacb5918432e7595e0edb6b74455a7a
kristobalus <ildar.tamendarov@gmail.com>
2024-09-05 21:51:47 +0500

    update

--- steering/service.py
+++ steering/service.py
@@ -6,6 +6,7 @@ from urllib.parse import urlparse
 import Jetson.GPIO as GPIO
 import paho.mqtt.client as mqtt
 
+
 # Get the configuration JSON from the environment variable
 config_json = os.getenv('CONFIGURATION')
 
@@ -17,10 +18,12 @@ else:
 
 # GPIO and PWM configuration
 neutral_position = int(config.get('neutral_position', 60))
-max_right = int(config.get('max_right', 70))
-max_left = int(config.get('max_left', 50))
+max_right_position = int(config.get('max_right_position', 70))
+max_left_position = int(config.get('max_left_position', 50))
 pin = int(config.get('pin', 15))
 frequency = int(config.get('frequency', 400))
+topic = int(config.get('topic'))
+
 
 # MQTT Configuration
 MQTT_BROKER = os.getenv('MQTT_BROKER')
@@ -30,58 +33,40 @@ MQTT_BROKER_PORT = int(parsed_url.port)
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.OUT)
-
 pwm = GPIO.PWM(pin, frequency)
 pwm.start(neutral_position)
 
 
-def neutral_to_left():
-    for duty_cycle in range(neutral_position, max_right, 1):
-        print(duty_cycle)
+def set_signal(value):
+    """
+    signal is sent by neuron network or by desktop application, range 0..1, 0.5 is the middle
+    """
+    delta = value - 0.5
+    if delta < 0:
+        # go left
+        dynamic_range = abs(neutral_position - max_left_position)
+        duty_cycle = neutral_position - abs(delta - 0.5) / 0.5 * dynamic_range
         pwm.ChangeDutyCycle(duty_cycle)
-        time.sleep(0.1)
-
-
-def left_to_neutral():
-    for duty_cycle in range(max_right, neutral_position, -1):
-        print(duty_cycle)
+    elif delta > 0:
+        # go right
+        dynamic_range = abs(neutral_position - max_right_position)
+        duty_cycle = neutral_position + abs(delta - 0.5) / 0.5 * dynamic_range
         pwm.ChangeDutyCycle(duty_cycle)
-        time.sleep(0.1)
-
-
-def neutral_to_right():
-    for duty_cycle in range(neutral_position, max_left, -1):
-        print(duty_cycle)
-        pwm.ChangeDutyCycle(duty_cycle)
-        time.sleep(0.1)
-
-
-def right_to_neutral():
-    for duty_cycle in range(max_left, neutral_position, 1):
-        print(duty_cycle)
-        pwm.ChangeDutyCycle(duty_cycle)
-        time.sleep(0.1)
 
 
 # MQTT event handlers
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     # Subscribe to a topic
-    client.subscribe("drive/command")
+    client.subscribe(topic)
 
 
 def on_message(client, userdata, msg):
     print("Message received on topic " + msg.topic + ": " + str(msg.payload))
-    command = msg.payload.decode()
 
-    if command == "neutral_to_left":
-        neutral_to_left()
-    elif command == "left_to_neutral":
-        left_to_neutral()
-    elif command == "neutral_to_right":
-        neutral_to_right()
-    elif command == "right_to_neutral":
-        right_to_neutral()
+    if msg.topic == topic:
+        value = float(msg.payload.decode())
+        set_signal(value)
     else:
         print("Unknown command")
 
