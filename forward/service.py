116a5f75fcf0f168db3713c4f20b1adad6a32478
kristobalus <ildar.tamendarov@gmail.com>
2024-09-07 17:41:26 +0500

    feat: forward motion

--- forward/service.py
+++ forward/service.py
@@ -1,7 +1,9 @@
 import json
 import os
+import threading
 import time
 from urllib.parse import urlparse
+from threading import Thread
 
 import Jetson.GPIO as GPIO
 import paho.mqtt.client as mqtt
@@ -36,27 +38,63 @@ GPIO.setup(pin, GPIO.OUT)
 pwm = GPIO.PWM(pin, frequency)
 pwm.start(full_stop)
 
-current_state = 0
+prev_value = 0.5
+motion_interval = 10
+motion_duty_cycle = full_stop
+motion_time = 0
+motion_time_lock = threading.Lock()  # Lock for thread-safe access
+motion_duty_cycle_lock = threading.Lock()  # Lock for thread-safe access
+motion_thread: Thread
 
 
-def go_stop(interval):
+def go_stop(duration):
     start_time = time.time()
-    while time.time() - start_time < interval:
+    while time.time() - start_time < duration:
         pwm.ChangeDutyCycle(full_stop)
 
 
-def go_forward(interval):
+def go_forward(duration):
     start_time = time.time()
-    while time.time() - start_time < interval:
+    while time.time() - start_time < duration:
         pwm.ChangeDutyCycle(full_forward)
 
 
-def go_backward(interval):
+def go_backward(duration):
+    global motion_time
     start_time = time.time()
-    pwm.ChangeDutyCycle(full_backward)
-    pwm.ChangeDutyCycle(full_stop)
-    while time.time() - start_time < interval:
+    while time.time() - start_time < duration:
         pwm.ChangeDutyCycle(full_backward)
+
+
+def motion_thread_handler():
+    start_time = time.time()
+    while True:
+        with motion_time_lock:
+            current_motion_time = motion_time  # Safely read the global motion_time
+        if time.time() - start_time >= current_motion_time:
+            break
+        pwm.ChangeDutyCycle(motion_duty_cycle)  # Simulate PWM action
+        # time.sleep(0.001)  # prevent high CPU overusage
+
+
+# Function to start go_forward in a thread
+def start_motion_thread():
+    global motion_thread
+    global motion_time
+    if motion_thread is None or motion_thread.is_alive() is False:
+        motion_thread = Thread(target=motion_thread_handler)
+        motion_thread.start()
+    else:
+        with motion_time_lock:
+            motion_time = motion_time + motion_interval
+
+
+def stop_motion_thread():
+    global motion_time
+    with motion_time_lock:
+        motion_time = 0
+    if motion_thread:
+        motion_thread.join()
 
 
 # MQTT event handlers
@@ -77,36 +115,64 @@ def on_message(client, userdata, msg):
         print("Unknown topic")
 
 
-def backward():
-    go_stop(0.1)
-    go_backward(0.1)
-    go_stop(0.1)
-    go_backward(1)
-    go_forward(0.1)
-
-
-def forward():
-    go_forward(1)
-
-
 def set_signal(value):
-    """
-    signal is sent by neuron network or by desktop application, range 0..1, 0.5 is the middle
-    """
-    global current_state
-    print(f"current_state={current_state}")
+    global motion_time
+    global prev_value
+    global motion_duty_cycle
+    if value > 0.5:
 
-    if value >= 0.51:
-        # forward motion
-        forward()
-    elif value <= 0.49:
+        if prev_value > 0.5:
+            # keep motion
+            start_motion_thread()
+
+        if prev_value < 0.5:
+            stop_motion_thread()
+            go_forward(0.1)
+            with motion_duty_cycle_lock:
+                motion_duty_cycle = full_forward
+            with motion_time_lock:
+                motion_time = motion_interval
+
+        if prev_value == 0.5:
+            with motion_duty_cycle_lock:
+                motion_duty_cycle = full_forward
+            with motion_time_lock:
+                motion_time = motion_interval
+            start_motion_thread()
+
+    if value == 0.5:
+        if prev_value < 0.5:
+            stop_motion_thread()
+            go_forward(0.1)
+            with motion_duty_cycle_lock:
+                motion_duty_cycle = full_stop
+            with motion_time_lock:
+                motion_time = motion_interval
+            start_motion_thread()
+        if prev_value > 0.5:
+            stop_motion_thread()
+            with motion_duty_cycle_lock:
+                motion_duty_cycle = full_stop
+            with motion_time_lock:
+                motion_time = motion_interval
+            start_motion_thread()
+
+    if value < 0.5:
         # backward motion
-        backward()
-    elif value == 0.5:
-        # brake
-        go_stop(0.1)
+        if prev_value < 0.5:
+            start_motion_thread()
+        if prev_value == 0.5 or prev_value > 0.5:
+            stop_motion_thread()
+            go_stop(0.1)
+            go_backward(0.1)
+            go_stop(0.1)
+            with motion_duty_cycle_lock:
+                motion_duty_cycle = full_backward
+            with motion_time_lock:
+                motion_time = motion_interval
+            start_motion_thread()
 
-    current_state = value
+    prev_value = value
 
 
 # MQTT client setup
@@ -115,8 +181,6 @@ client.on_connect = on_connect
 client.on_message = on_message
 client.connect(MQTT_BROKER_HOST, MQTT_BROKER_PORT, 30)
 
-# Blocking call to process network traffic, dispatch callbacks, and handle reconnecting.
-# It will run forever unless an exception occurs or a signal is sent to stop it.
 try:
     client.loop_forever()
 finally:
